e17c66d2e3d9b9ad67cfd2600e049a2a7b89d1a1
thobranifurqon <thobranifurqon@gmail.com>
2023-08-29 11:44:53 +0800

    New Update Of Header React-App

---
+++ build/precache-manifest.e7ec29fdeb51a066ae5d8d0733d31b5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "02ee0e4a75354a630dd4",
+    "url": "/static/css/main.23d37159.chunk.css"
+  },
+  {
+    "revision": "02ee0e4a75354a630dd4",
+    "url": "/static/js/main.02ee0e4a.chunk.js"
+  },
+  {
+    "revision": "8c30412421a2fa73ad91",
+    "url": "/static/js/1.8c304124.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "5b847aab52010a21ca557ec5891e190c",
+    "url": "/index.html"
+  }
+];
--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <p> <h3> Welcome to Daru Thobrani Furqon profile </h3></p>
       </div>
     );
   }
